7f3dcf32cbef916012a8f45a66fb20ec44834136
Convolutio <79534557+Convolutio@users.noreply.github.com>
2024-02-27 17:46:46 +0100

    removed indent parametre

--- config.lua
+++ config.lua
@@ -4,58 +4,58 @@
 -- Discord: https://discord.com/invite/Xb9B4Ny
 
 vim.opt.number = true
-vim.opt.shiftwidth = 4
-vim.opt.tabstop = 4
+-- vim.opt.shiftwidth = 4
+-- vim.opt.tabstop = 4
 vim.opt.guifont = "FiraCode Nerd Font:h11"
 local TEXTWIDTH = 79
 vim.opt.colorcolumn = tostring(TEXTWIDTH + 1)
 vim.opt.textwidth = TEXTWIDTH
 
 lvim.plugins = {
-    {
-        "catppuccin/nvim",
-        config = function()
-            require("catppuccin").setup({
-                background = {
-                    dark = "macchiato",
-                    light = "latte"
-                },
-                term_colors = true,
-                integrations = {
-                    cmp = true,
-                    treesitter = true
-                }
-            })
-        end
-    },
-    {
-        "iamcco/markdown-preview.nvim",
-        build = "cd app && npm install",
-        ft = "markdown",
-        config = function()
-            vim.g.mkdp_auto_start = 1
-        end,
-    },
-    {
-        "sindrets/diffview.nvim",
-        event = "BufRead",
-    },
-    {
-        "AckslD/swenv.nvim"
-    },
-    {
-        "andweeb/presence.nvim",
-        config = function()
-            require("presence").setup({
-                blacklist = { ".*serv_bda.*" },
-            })
-        end
-    }
+  {
+    "catppuccin/nvim",
+    config = function()
+      require("catppuccin").setup({
+        background = {
+          dark = "macchiato",
+          light = "latte"
+        },
+        term_colors = true,
+        integrations = {
+          cmp = true,
+          treesitter = true
+        }
+      })
+    end
+  },
+  {
+    "iamcco/markdown-preview.nvim",
+    build = "cd app && npm install",
+    ft = "markdown",
+    config = function()
+      vim.g.mkdp_auto_start = 1
+    end,
+  },
+  {
+    "sindrets/diffview.nvim",
+    event = "BufRead",
+  },
+  {
+    "AckslD/swenv.nvim"
+  },
+  {
+    "andweeb/presence.nvim",
+    config = function()
+      require("presence").setup({
+        blacklist = { ".*bda.*" },
+      })
+    end
+  }
 }
 
 
 lvim.colorscheme = "catppuccin"
 
-vim.cmd("setlocal tabstop=4 shiftwidth=4")
+-- vim.cmd("setlocal tabstop=4 shiftwidth=4")
 lvim.format_on_save.enabled = true
 lvim.format_on_save.pattern = { "*.lua", "*.py" }
