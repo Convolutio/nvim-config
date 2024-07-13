9b2ca16e7ecacdbf080f89bed4a66ffb16dbfcd0
Convolutio <79534557+Convolutio@users.noreply.github.com>
2024-07-13 08:20:51 +0200

    feat: manual clipboard by default

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,7 +3,14 @@
 -- Add any additional options here
 
 local opt = vim.opt
+
 opt.textwidth = 79
 opt.colorcolumn = "+1"
+
 opt.wrap = true
+
+-- `+y` for system yank, `y` for in session yank
+opt.clipboard = ""
+
+-- remove format on file save
 vim.g.autoformat = false
