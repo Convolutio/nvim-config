692d6fd61c667d8dae6913e43b1189e2538cbc46
Convolutio <79534557+Convolutio@users.noreply.github.com>
2024-12-26 15:09:41 +0100

    feat: manually setup clangd

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Clone this branch instead of the `~/.config/nvim` directory.
 ## Requirements
 
 - chafa
+- clangd installed manually (`guix install clangd`)
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,8 +1,50 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    opts = {
-      inlay_hints = { enabled = false },
-    },
+    opts = function(_, opts)
+      local inlay_hints = opts.inlay_hints or {}
+      inlay_hints.enabled = false
+      opts.inlay_hints = inlay_hints
+
+      local clangd_opts = opts.servers.clangd
+        or {
+          keys = {
+            { "<leader>ch", "<cmd>ClangdSwitchSourceHeader<cr>", desc = "Switch Source/Header (C/C++)" },
+          },
+          root_dir = function(fname)
+            return require("lspconfig.util").root_pattern(
+              "Makefile",
+              "configure.ac",
+              "configure.in",
+              "config.h.in",
+              "meson.build",
+              "meson_options.txt",
+              "build.ninja"
+            )(fname) or require("lspconfig.util").root_pattern("compile_commands.json", "compile_flags.txt")(
+              fname
+            ) or require("lspconfig.util").find_git_ancestor(fname)
+          end,
+          capabilities = {
+            offsetEncoding = { "utf-16" },
+          },
+          cmd = {
+            "clangd",
+            "--background-index",
+            "--clang-tidy",
+            "--header-insertion=iwyu",
+            "--completion-style=detailed",
+            "--function-arg-placeholders",
+            "--fallback-style=llvm",
+          },
+          init_options = {
+            usePlaceholders = true,
+            completeUnimported = true,
+            clangdFileStatus = true,
+          },
+        }
+      clangd_opts.cmd[1] = "/home/thormas/.guix-profile/bin/clangd"
+      clangd_opts.mason = false
+      opts.servers.clangd = clangd_opts
+    end,
   },
 }
